5100fe97990dfa5fdb46bb45af87cb60c6f275d2
smeleyka <smeleyka@gmail.com>
2018-03-05 15:45:09 +0300

    Init

--- app/src/main/java/ru/smeleyka/pblesson2/MainActivity.java
+++ app/src/main/java/ru/smeleyka/pblesson2/MainActivity.java
@@ -2,84 +2,40 @@ package ru.smeleyka.pblesson2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 import com.jakewharton.rxbinding2.widget.RxTextView;
-
-import org.reactivestreams.Subscription;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import io.reactivex.Observable;
-import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.edit_text)
-    EditText editText;
-    @BindView(R.id.button)
-    Button button;
-    @BindView(R.id.view_text)
-    TextView textView;
-
+    @BindView(R.id.edit_text)    EditText editText;
+    @BindView(R.id.view_text)    TextView textView;
+    protected Disposable editTextSubscr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        createRx();
-        observeEditText();
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        Subscription editTextSubscr = RxTextView.textChanges(editText).subscribe(new Action1<CharSequence>() {
-            @Override
-            public void call(CharSequence value) {
-                textView.setText(value);
-            }
-        });
+        onEditTextChange();
     }
 
-    private void observeEditText() {
-        Observable<String> obseravableEditText = new Observable<String>() {
-            @Override
-            protected void subscribeActual(Observer<? super String> observer) {
+    private void onEditTextChange() {
+        Disposable editTextSubscr = RxTextView.textChanges(editText)
+                .subscribe(charSequence -> textView.setText(charSequence));
 
-            }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (!editTextSubscr.isDisposed()){
+            editTextSubscr.dispose();
         }
     }
-
-
-    @OnClick(R.id.button)
-    private void onButtonClick() {
-
-    }
-
-    private void createRx() {
-
-    }
-
-
 }
